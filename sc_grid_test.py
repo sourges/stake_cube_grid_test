8761b2f6ce1498a8bac378f299f367f2a8c032d3
sourges <107276414+sourges@users.noreply.github.com>
2022-07-20 14:30:54 -0400

    Add files via upload
    some clean up

--- sc_grid_test.py
+++ sc_grid_test.py
@@ -1,5 +1,4 @@
-# this whole thing needs cleaning up lmao
-
+# clean this whole mess up
 
 
 import hmac
@@ -30,20 +29,6 @@ def get_order_info():
 	response = requests.get(url, headers = headers)
 	#print(response.json()[])
 	return response.json()['result']
-
-# orders = get_order_info()  # - active with id
-# active_orders = []
-# sell_orders = [12345, 3454566, 4087354]
-
-# for count in range(len(orders['result'])):
-# 	active_orders.append(orders['result'][count]['id'])
-# print(active_orders)
-# for i in sell_orders:
-# 	if i not in active_orders:
-# 		new_buy_price = float(order_info['price']) - grid_size
-
-		
-
 
 
 
@@ -76,19 +61,9 @@ def place_order(price, position_size, side):
 	return response.json()
 
 
-# price = 0.003
-# order = place_order(price, config.position_size, side = "SELL")
-# print("printing outside")
-# print(order)
-# sell_orders = []
-# sell_orders.append(order['result']['orderId'])
-# print(f"inside sell_orders - {sell_orders}")
 
 
 #get_order_info()
-
-# sell_order = get_order_info(sell_orders['orderId'])
-# print(sell_order)
 
 
 # doesnt have orderId
@@ -115,24 +90,6 @@ def my_trades():
 	return response.json()
 
 
-#my_trades()
-#get_order_info()
-
-
-# closed_trades = my_trades()
-# orders = get_order_info()
-
-# for order in orders['result']:
-# 	for count in range(len(closed_trades['result'])):
-# 		if order['id'] == closed_trades['result'][count]['id']:
-# 			print("trade is closed")
-# 			print(order['price'])
-
-
-	
-
-
-
 
 def get_single_ticker():
 	url = "https://stakecube.io/api/v2/exchange/spot/orderbook?market=BITB_DOGE"
@@ -153,25 +110,21 @@ def get_single_ticker():
 
 # get_single_ticker()
 
-# # will take out prints later - using them for testing right now
+# will take out prints later - using them for testing right now
 def test_grid():
-	current_price = get_single_ticker()
-# 	current_price = float(current_price)
-# 	curren_price = round(current_price, 3)
+
+	current_price = get_single_ticker()  # to get the median price
 	buy_orders = []
 	sell_orders = []
 
 
-	# 	# sell grid
+	# sell grid
 	for i in range(config.number_sell_gridlines):
 		price = current_price + (config.grid_size * (i+1))
 		price = round(price, 8)
 		#print(price)
 		time.sleep(1)
 		order = place_order(price, config.position_size, side = "SELL")
-
-		
-		
 
 		sell_orders.append(order['result'])
 
@@ -190,7 +143,7 @@ def test_grid():
 
 
 
-# # 	# buy grid
+ 	# buy grid
 	for i in range(config.number_buy_gridlines):
 		price = current_price - (config.grid_size * (i+1))
 		price = round(price, 8)
@@ -205,34 +158,25 @@ def test_grid():
 	return sell_orders, buy_orders
 
 
-sell_orders, buy_orders = test_grid()   # - upon restart take this out and test what it does before the loop starts - ***********************
+sell_orders, buy_orders = test_grid()  
 closed_orders = []
-# active_orders = []
-# orders = get_order_info()
-# for count in range(len(orders['result'])):
-# 	active_orders.append(orders['result'][count])
 
 
-
-
-
-
-
-
-#closed_trades = my_trades()                     #  ['orderId'] - closed
-orders = get_order_info()                       # ['id'] - open
-#print(orders)									# ['orderId'] - sell_orders
+# closed_trades = my_trades() # ['orderId'] 
+orders = get_order_info()     # ['id'] - open
+# sell_orders, buy_orders     # ['orderId'] 
 
 #print(closed_trades['result'])
 
 # closed orderId
 
+
+# all prints in this loop for testing, will give more meaningful info later / take out random prints
 while True:
 	
 	closed_trades = my_trades()
 	print("*****************************************")
-	#print(closed_trades['result'])
-	#for order in orders:
+	
 	count = 0
 	for sell_order in sell_orders:
 		for i in range(len(closed_trades['result'])):
@@ -255,8 +199,6 @@ while True:
 				print(sell_orders)
 				break
 		count +=1		
-
-
 
 
 	count = 0
@@ -282,96 +224,5 @@ while True:
 				break
 		count +=1	
 
-
-
 	print("pausing")
 	time.sleep(5)
-
-# if order_info['isActive'] == closed_order_status:
-# 			closed_order_ids.append(order_info['id'])
-			
-# 			print(f"Closed Orders - {closed_order_ids}")
-
-# 			new_buy_price = float(order_info['price']) - grid_size
-# 			new_buy_order = place_order(new_buy_price, position_size, "BUY")
-# 			buy_orders.append(new_buy_order['data'])
-
-
-
-
-
-
-
-
-
-
-	# for sell_order in sell_orders:
-	# 	for active_order in active_orders:
-	# 		if sell_order['orderId'] == active_order['id']:
-	# 			print("in active order")
-	# print("pausing")
-	# time.sleep(5)
-
-# while True:
-# 	closed_order_ids = []
-# 	active_orders = []
-	
-# 	orders = get_order_info()
-# 	for i in range (len(orders['result']))
-# 	active_orders.append(orders['result'][i]['id'])
-
-# 	for sell_order in sell_orders:
-# 		if sell_order in active_orders['result']
-
-# 		print("checking sells")
-# 		count = 0
-# 		for count in range(len(active_orders['result'])):
-# 			if orders['result'][count]['id'] == sell_order:
-# 				print("good")
-# 			count += 1
-
-# 		if order_info['isActive'] == closed_order_status:
-# 			closed_order_ids.append(order_info['id'])
-			
-# 			print(f"Closed Orders - {closed_order_ids}")
-
-# 			new_buy_price = float(order_info['price']) - grid_size
-# 			new_buy_order = place_order(new_buy_price, position_size, "BUY")
-# 			buy_orders.append(new_buy_order['data'])
-
-			
-
-# # need to make sure this append works - print out buy_ourders again with new append
-
-
-# 		time.sleep(check_orders_frequency)
-
-# 	for buy_order in buy_orders:
-# 		try:
-# 			order = get_order_info(buy_order['orderId'])
-# 		 	# print(order)
-
-# 			order_info = order['data']
-# 		except Exception as e:
-# 			print("error in #2")
-# 			continue
-
-# 		if order_info['isActive'] == closed_order_status:
-# 			closed_order_ids.append(order_info['id'])
-
-# 			print(f"Closed Orders - {closed_order_ids}")
-
-# 			new_sell_price = float(order_info['price']) + grid_size
-# 			new_sell_order = place_order(new_sell_price, position_size, "SELL")
-# 			sell_orders.append(new_sell_order['data'])
-
-# 		time.sleep(check_orders_frequency)
-
-	
-# 	for order_id in closed_order_ids:
-# 		buy_orders = [buy_order for buy_order in buy_orders if buy_order['orderId'] != order_id]
-# 		print(f" Buy list - {buy_orders}")
-		
-# 		sell_orders = [sell_order for sell_order in sell_orders if sell_order['orderId'] != order_id]
-# 		print(f" Sell list - {sell_orders}")
-
