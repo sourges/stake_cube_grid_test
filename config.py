a7158450f0088cef2581c84111789af483caf78b
sourges <107276414+sourges@users.noreply.github.com>
2022-08-03 10:38:48 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+KEY = ''
+SECRET = ''
+
+
+position_size = 40
+
+# gridbot settings
+number_buy_gridlines = 8
+number_sell_gridlines = 8
+
+
+grid_size = 0.00000050 # SCC-BTC
+
+#grid_size = .00012 # BITB
+# BITB - .00015
+# DASH - .0001
+
+check_orders_frequency = 1
+
+#closed_order_status = False  - probably delete
+
