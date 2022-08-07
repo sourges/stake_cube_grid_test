e6e55945de9767aab46dfede41649a2f18e1b190
sourge <107276414+sourges@users.noreply.github.com>
2022-08-07 09:22:06 -0400

    deleted unused variables

--- config.py
+++ config.py
@@ -2,20 +2,21 @@ KEY = ''
 SECRET = ''
 
 
-position_size = 40
+position_size = 70
+
+trading_pair = "BITB_DOGE" # example
 
 # gridbot settings
-number_buy_gridlines = 8
-number_sell_gridlines = 8
+number_buy_gridlines = 10
+number_sell_gridlines = 10
 
 
-grid_size = 0.00000050 # SCC-BTC
-
-#grid_size = .00012 # BITB
+#grid_size = 0.00000025 # SCC-BTC
+grid_size = 0.00010
+#grid_size = .000070 # BITB
 # BITB - .00015
 # DASH - .0001
 
-check_orders_frequency = 1  # dont need - check later
 
-#closed_order_status = False  - probably delete
+
 
